63122244cb2fffb8debdb0e222b0ed6f3478db33
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2014-11-06 23:15:58 +0000

    updated clean scripts
    git-svn-id: file:///var/local/svn/basicwriter@100 daaaf23c-2e50-4459-9457-1e69db5a47bf

--- clean.sh
+++ clean.sh
@@ -2,4 +2,6 @@
 
 find build/ -depth -mindepth 1 -delete -print
 
+rm -Rf CMakeFiles
+
 find -depth \( \( -name "*.aux" -o -name "*.snm" -o -name "*.out" -o -name "*.toc" -o -name "*.nav" -o -name "*.log" -o -name "*.backup" -o -name "*.bbl" -o -name "*.blg" -o -name "*~" -o -name "CMakeCache.txt" \) -a \! \( -wholename "*.git/*" -o -wholename "*.svn" \) \) -delete -print
