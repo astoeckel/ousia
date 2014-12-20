55b860964cb752b920d415c4332da85a97abd087
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2014-12-20 15:48:25 +0100

    added small utf8cpp library for encoding unicode characters as UTF8, updated download_dependencies.sh

--- lib/download_dependencies.sh
+++ lib/download_dependencies.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 # Download and unzip googletest
-wget https://googletest.googlecode.com/files/gtest-1.7.0.zip
-unzip gtest-1.7.0.zip
+if [ ! -e gtest-1.7.0.zip ]; then
+	wget https://googletest.googlecode.com/files/gtest-1.7.0.zip
+	unzip gtest-1.7.0.zip
+fi
 
+# Download utf8-cpp (header only library)
+if [ ! -e utf8cpp ]; then
+	svn checkout svn://svn.code.sf.net/p/utfcpp/code/v2_0/source utf8cpp
+fi
