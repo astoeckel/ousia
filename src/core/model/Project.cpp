677d6830a0252e7478c2f5d6f75cfb845d33ad23
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-01-18 21:09:57 +0100

    Added Project class which represents the absolute top-level node containing multiple documents and all loaded domains and typesystems.

---
+++ src/core/model/Project.cpp
@@ -0,0 +1,95 @@
+/*
+    Ousía
+    Copyright (C) 2014, 2015  Benjamin Paaßen, Andreas Stöckel
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <core/common/RttiBuilder.hpp>
+
+#include "Domain.hpp"
+#include "Document.hpp"
+#include "Project.hpp"
+#include "Typesystem.hpp"
+
+namespace ousia {
+
+namespace model {
+
+Project::Project(Manager &mgr)
+    : Node(mgr),
+      systemTypesystem(acquire(new SystemTypesystem(mgr))),
+      documents(this),
+      domains(this),
+      typesystems(this)
+{
+}
+
+bool Project::doValidate(Logger &logger) const
+{
+	return continueValidation(documents, logger) &
+	       continueValidation(domains, logger) &
+	       continueValidation(typesystems, logger);
+}
+
+Rooted<Typesystem> Project::createTypesystem(const std::string &name)
+{
+	Rooted<Typesystem> typesystem{
+	    new Typesystem{getManager(), systemTypesystem, name}};
+	addTypesystem(typesystem);
+	return typesystem;
+}
+
+void Project::addTypesystem(Handle<Typesystem> typesystem)
+{
+	invalidate();
+	typesystems.push_back(typesystem);
+}
+
+Rooted<Document> Project::createDocument(const std::string &name)
+{
+	Rooted<Document> document{new Document(getManager(), name)};
+	addDocument(document);
+	return document;
+}
+
+void Project::addDocument(Handle<Document> document)
+{
+	invalidate();
+	documents.push_back(document);
+}
+
+Rooted<Domain> Project::createDomain(const std::string &name)
+{
+	Rooted<Domain> domain{new Domain(getManager(), systemTypesystem, name)};
+	addDomain(domain);
+	return domain;
+}
+
+void Project::addDomain(Handle<Domain> domain)
+{
+	invalidate();
+	domains.push_back(domain);
+}
+}
+
+namespace RttiTypes {
+const Rtti Project = RttiBuilder<model::Project>("Project")
+                         .parent(&Node)
+                         .composedOf(&Document)
+                         .composedOf(&Typesystem)
+                         .composedOf(&Domain);
+}
+}
+
---
+++ src/core/model/Project.hpp
@@ -0,0 +1,171 @@
+/*
+    Ousía
+    Copyright (C) 2014, 2015  Benjamin Paaßen, Andreas Stöckel
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file Project.hpp
+ *
+ * Contains the concept of the "Project" class which represents the entity into
+ * which domains, documents, typesystems and other resources are embedded.
+ *
+ * @author Andreas Stöckel (astoecke@techfak.uni-bielefeld.de)
+ */
+
+#ifndef _OUSIA_PROJECT_HPP_
+#define _OUSIA_PROJECT_HPP_
+
+#include "Node.hpp"
+
+namespace ousia {
+
+// Forward declarations
+class Logger;
+class Rtti;
+class SystemTypesystem;
+class Typesystem;
+
+namespace model {
+
+/**
+ * The Project class constitutes the top-level node in which documents, domains,
+ * typesystems and other resources are embedded.
+ */
+class Project : public Node {
+private:
+	/**
+	 * Private instance of the system typesystem which is distributed as a
+	 * reference to all child typesystems.
+	 */
+	Owned<SystemTypesystem> systemTypesystem;
+
+	/**
+	 * List containing all loaded documents.
+	 */
+	NodeVector<Document> documents;
+
+	/**
+	 * List containing all loaded domains.
+	 */
+	NodeVector<Domain> domains;
+
+	/**
+	 * List containing all loaded typesystems.
+	 */
+	NodeVector<Typesystem> typesystems;
+
+protected:
+	/**
+	 * Validates the project and all parts it consists of.
+	 *
+	 * @param logger is the logger instance to which errors will be logged.
+	 */
+	bool doValidate(Logger &loger) const override;
+
+public:
+	/**
+	 * Constructor of the Project class.
+	 *
+	 * @param mgr is the manager instance used for managing this Node.
+	 */
+	Project(Manager &mgr);
+
+	/**
+	 * Returns a reference to the internal system typesystem.
+	 *
+	 * @return a reference to the system typesystem.
+	 */
+	Rooted<SystemTypesystem> getSystemTypesystem() { return systemTypesystem; }
+
+	/**
+	 * Returns a new typesystem with the given name adds it to the list of
+	 * typesystems. Provides a reference of the system typesystem to the
+	 * typesystem.
+	 *
+	 * @param name is the name of the typesystem that should be created.
+	 */
+	Rooted<Typesystem> createTypesystem(const std::string &name);
+
+	/**
+	 * Adds a single new typesystem to the project.
+	 *
+	 * @param typesystem is the typesystem that should be added to the project.
+	 */
+	void addTypesystem(Handle<Typesystem> typesystem);
+
+	/**
+	 * Returns a new document with the given name and adds it to the list of
+	 * documents.
+	 *
+	 * @param name is the name of the document that should be created.
+	 */
+	Rooted<Document> createDocument(const std::string &name);
+
+	/**
+	 * Adds the given document to the list of documents in the project.
+	 *
+	 * @param document is the document that should be added to the project.
+	 */
+	void addDocument(Handle<Document> document);
+
+	/**
+	 * Returns a new domain with the given name and adds it to the list of
+	 * domains. Provides a reference of the system typesystem to the domain.
+	 *
+	 * @param name is the name of the domain that should be created.
+	 */
+	Rooted<Domain> createDomain(const std::string &name);
+
+	/**
+	 * Adds the given domain to the list of domains in the project.
+	 *
+	 * @param domain is the document that should be added to the project.
+	 */
+	void addDomain(Handle<Domain> domain);
+
+	/**
+	 * Returns all documents of this project.
+	 *
+	 * @return a reference pointing at the document list.
+	 */
+	const NodeVector<Document> &getDocuments() { return documents; }
+
+	/**
+	 * Returns all domains of this project.
+	 *
+	 * @return a reference pointing at the domain list.
+	 */
+	const NodeVector<Domain> &getDomains() { return domains; }
+
+	/**
+	 * Returns all typesystems of this project.
+	 *
+	 * @return a reference pointing at the typesystem list.
+	 */
+	const NodeVector<Typesystem> &getTypesystems() { return typesystems; }
+};
+}
+
+namespace RttiTypes {
+/**
+ * Type information of the Project class.
+ */
+extern const Rtti Project;
+}
+}
+
+#endif /* _OUSIA_PROJECT_HPP_ */
+
