fca50d3411005d32b2621b4f1886be35d6b73bec
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-01-25 21:39:58 +0100

    Fixed compilation

--- src/core/resource/Resource.cpp
+++ src/core/resource/Resource.cpp
@@ -16,6 +16,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <unordered_map>
+#include <unordered_set>
+
 #include <core/common/Utils.hpp>
 
 #include "Resource.hpp"
