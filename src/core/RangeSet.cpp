bfdfc1011b2571906f4368e589aeb034216b16ad
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-06-12 00:07:24 +0200

    Update copyright

--- src/core/RangeSet.cpp
+++ src/core/RangeSet.cpp
@@ -1,6 +1,6 @@
 /*
     Ousía
-    Copyright (C) 2014  Benjamin Paaßen, Andreas Stöckel
+    Copyright (C) 2014, 2015  Benjamin Paaßen, Andreas Stöckel
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
